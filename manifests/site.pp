68af28f4935d36ae859b5e3ceb89020a97a774d4
tesks <111441006+tesks@users.noreply.github.com>
2022-08-20 18:02:19 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,5 @@
 node default{
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is readme...',
-    owner   => 'root',
-  }
-  #file {'/root/README':
-  #  owner => 'root',
-  #}
+}
+node 'master.puppet.vm'{
+  include role::master_server
 }
