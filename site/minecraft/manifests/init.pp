c7dfac0d1a1fcbbf71e2c31fc7a7a314de14022d
tesks <111441006+tesks@users.noreply.github.com>
2022-09-09 07:51:25 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -4,6 +4,18 @@ class minecraft {
   }
   file {'/opt/minecraft/server.jar'
     ensure => file,
-    source => 
+    source => 'https://s3.amazonaws.com/Mincraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
   }
+  package {'java':
+    ensure => present,
+  }
+  file {'/opt/minecraft/eula.txt':
+    ensure => file,
+    content => 'eula=true',
+  }
+  file {'/etc/systemd/system/minecraft.service':
+    ensure => file,
+    source => 'puppet:///module/minecraft/minecraft.service',
+  }
+  # Note: "puppet:///", then defaults to the master. "puppet://<alternate file share>/" can be specified, but rarely done.
 }
